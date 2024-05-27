d0459d08896ccc03198c3b0eb21f4a14fb98e17f
Linetgithub <awinoojwang22@gmail.com>
2024-05-27 19:13:33 +0300

    error fixing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const express =require('express');
 const axios = require('axios');
+const { json } = require('body-parser');
 require('dotenv').config();
 
 const app = express();
@@ -7,23 +8,29 @@ const port = 4000;
 
 const API_KEY = process.env.API_KEY;
 
+app.use(json());
+
 app.get('/', (req, res) => {
   const address = req.query.address;
-  const url = 'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=fejStCbUhO81QE4GppmJsWMayxIqtIgH';
+  const url = 'https://api.tomorrow.io/v4/weather/forecast?location=9.0820,8.6753&apikey=fejStCbUhO81QE4GppmJsWMayxIqtIgH';
   console.log(url)
 
   console.log('Requesting URL:', url);
 
   axios.get(url)
     .then(response => {
-      const data = response.data;
-      const cityName = data.name;
-      const temperature = data.main.temp;
-      const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
-      const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
+        const data = response.data;
+        const dailyData = data.timelines.dily[0];
+        //
+        console.log(dailyData);
+        const cityName = "Nigeria";
+        const temperature = dailyData.values.temperatureAvg;
+        const sunsetTime = new Date(sunsetTimeString).toISOString().slice(11,16);
+        const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
+        
+        res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
+})
 
-      res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
-    })
     .catch(error => {
       console.error(error);
       res.status(500).send('Error occurred while fetching weather data');
